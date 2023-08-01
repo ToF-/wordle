06707cf726ccd06bfa93375e873341aaa91304b7
Christophe Thibaut <cthibaut@octo.com>
2023-08-01 22:57:30 +0200

    print result with ansi term colors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-fn main() {
-    println!("Hello, world!");
-}
+use ansi_term::*;
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub enum Status {
@@ -13,18 +11,50 @@ type Letter = (char, Status);
 
 pub fn play(trial: &String, secret: &String) -> Vec<Letter> {
     let mut result:Vec<Letter> = trial.chars().map(|c| (c, Status::Absent) ).collect();
+    let mut checked:Vec<bool> = vec![false, false, false, false, false];
+    // first look for all correct letters
     for (i, c) in trial.chars().enumerate() {
-        if let Some(p) = secret.find(c) {
-            if i == p {
-                result[i] = (c, Status::Correct)
-            } else {
-                result[i] = (c, Status::Misplaced)
+        if let Some(ch) = secret.chars().nth(i) { 
+            if c == ch {
+                result[i] = (c, Status::Correct);
+                checked[i] = true;
             }
         }
     }
-    result.sort_by(|(a,_), (b,_)|  a.cmp(b));
+    // then look for all misplaced letters
+    for (i, c) in trial.chars().enumerate() {
+        if !checked[i] {
+            for (j, d) in secret.chars().enumerate() {
+                if !checked[j] && d == c {
+                    result[i] = (c, Status::Misplaced);
+                    checked[j] = true;
+                }
+
+            }
+        }
+    }
     result
 }
+
+pub fn show_result(result: Vec<Letter>) {
+    let mut styles: Vec<Style> = vec![Colour::White.normal();5];
+    for (i, (c,s)) in result.iter().enumerate() {
+        styles[i] = match s {
+            Status::Absent => Color::White.normal().dimmed().on(Color::Black),
+            Status::Misplaced => Color::White.normal().bold().on(Color::Yellow),
+            Status::Correct => Color::White.normal().bold().on(Color::Green),
+        };
+        print!("{}",styles[i].paint(c.to_string()));
+    }
+    println!("");
+
+}
+fn main() {
+    show_result(play(&"READY".to_string(),&"BINGO".to_string()));
+    show_result(play(&"GREAT".to_string(),&"BINGO".to_string()));
+    show_result(play(&"SMALL".to_string(),&"GRAAL".to_string()));
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -35,10 +65,10 @@ mod tests {
         let trial: String = String::from("READY");
         let secret: String =String::from("BINGO");
         assert_eq!(play(&trial, &secret), 
-                   vec![('A', Status::Absent),
-                        ('D', Status::Absent),
+                   vec![('R', Status::Absent),
                         ('E', Status::Absent),
-                        ('R', Status::Absent),
+                        ('A', Status::Absent),
+                        ('D', Status::Absent),
                         ('Y', Status::Absent)])
     }
 
@@ -47,10 +77,10 @@ mod tests {
         let trial: String = String::from("GREAT");
         let secret: String =String::from("BINGO");
         assert_eq!(play(&trial, &secret), 
-                   vec![('A', Status::Absent),
-                        ('E', Status::Absent),
-                        ('G', Status::Misplaced),
+                   vec![('G', Status::Misplaced),
                         ('R', Status::Absent),
+                        ('E', Status::Absent),
+                        ('A', Status::Absent),
                         ('T', Status::Absent)])
     }
     #[test]
@@ -58,10 +88,22 @@ mod tests {
         let trial: String = String::from("READY");
         let secret: String =String::from("BINGY");
         assert_eq!(play(&trial, &secret), 
-                   vec![('A', Status::Absent),
-                        ('D', Status::Absent),
+                   vec![('R', Status::Absent),
                         ('E', Status::Absent),
-                        ('R', Status::Absent),
+                        ('A', Status::Absent),
+                        ('D', Status::Absent),
                         ('Y', Status::Correct)])
     }
+    #[test]
+    fn found_one_correct_and_one_misplaced_letters() {
+        let trial: String = String::from("SMALL");
+        let secret: String =String::from("GRAAL");
+        assert_eq!(play(&trial, &secret), 
+                   vec![('S', Status::Absent),
+                        ('M', Status::Absent),
+                        ('A', Status::Correct),
+                        ('L', Status::Absent),
+                        ('L', Status::Correct)])
+    }
+
 }
